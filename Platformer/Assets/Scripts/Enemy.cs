7525fe4152e6316c9298db57880f6eca77198ec2
Mail1852010 <liam.marth@gmail.com>
2023-12-28 18:40:56 +0100

    GegnerUpdate

---
+++ Platformer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5f;
+    public bool isPlayer;
+    private Vector3 rotation;
+    private Vector3 currentPosition;
+
+    private GameObject player;
+    private Transform playerTransform;
+    private Player playerScript;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerTransform = GameObject.Find("Player").transform;
+        playerScript = playerTransform.GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        if (!isPlayer)
+        {
+            Debug.Log("NormalEnemy");
+        }
+        else
+        {
+            player.SetActive(false);
+            playerTransform.position = currentPosition;
+            float richtung = Input.GetAxis("Horizontal");
+            if (richtung < 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                transform.Translate(Vector2.right * speed * -richtung * Time.deltaTime);
+            }
+            if (richtung > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+            }
+        }
+
+        currentPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isPlayer = true;
+        }
+    }
+}
--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -54,7 +54,7 @@ public class Player : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "ground")
+        if(collision.gameObject.tag == "Tilemap")
         {
             isGrounded = true;
         }
